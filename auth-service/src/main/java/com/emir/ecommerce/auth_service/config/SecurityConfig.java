63fc73ccfe4aa42532672492f1f223cf6acfe1c4
emirnar2701@gmail.com <emirnar2701@gmail.com>
2025-07-12 19:19:59 +0300

    docker configuration of auth service implemented

--- auth-service/src/main/java/com/emir/ecommerce/auth_service/config/SecurityConfig.java
+++ auth-service/src/main/java/com/emir/ecommerce/auth_service/config/SecurityConfig.java
@@ -48,7 +48,7 @@ public class SecurityConfig {
     @Bean
     public AuthenticationProvider authenticationProvider() {
         DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
-        authProvider.setUserDetailsService(userDetailsService);  // Burada kullanılıyor
+        authProvider.setUserDetailsService(userDetailsService);
         authProvider.setPasswordEncoder(passwordEncoder());
         return authProvider;
     }
